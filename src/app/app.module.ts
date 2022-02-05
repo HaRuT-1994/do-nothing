1c4cee4e23333e1bab39c1f61540c5a500b55cf5
Harut <harut1225@gmail.com>
2022-02-05 22:08:35 +1100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoNothingComponent } from './do-nothing/do-nothing.component';
+import { ConfigScenariosComponent } from './config-scenarios/config-scenarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoNothingComponent,
+    ConfigScenariosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
